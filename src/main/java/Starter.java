b6e57aa9f1bb12abc510ae5f4e19f17562f3ff98
Oshadha <hpgmoshadhapalliyaguru@gmail.com>
2025-11-17 02:55:20 +0530

    login UI Created

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,15 +1,18 @@
-package edu.icet;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        launch();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login.fxml"))));
+        stage.show();
     }
 }
---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private JFXTextField name_txtField;
+
+    @FXML
+    private JFXTextField password_txtField;
+
+    @FXML
+    void buttonLogin(ActionEvent event) {
+
+    }
+
+}
