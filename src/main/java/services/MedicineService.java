ae38808f1680e4eaa87ffb7f665ec93c138c1d24
Oshadha <moshadha23@gmail.com>
2026-01-07 23:26:43 +0530

    add MedicineService interface for managing medicine-related operations

---
+++ src/main/java/services/MedicineService.java
@@ -0,0 +1,19 @@
+package services;
+
+import javafx.collections.ObservableList;
+import model.dto.CartItem;
+import model.dto.Medicine;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface MedicineService {
+
+    boolean addMedicine(Medicine medicine);
+    ObservableList<Medicine>  searchByName(String name);
+    List<Medicine> getAllMedicines();
+    boolean Update(Medicine medicine);
+    boolean deleteMedicine(String name) throws SQLException;
+    void updateMedicineQty(ObservableList<CartItem> cartItems);
+    boolean UpdateSupplierQyt(String medicineId, int qty);
+}
