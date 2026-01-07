97b97276685e6d67bbdd3964ae07842a01a0deab
Oshadha <moshadha23@gmail.com>
2026-01-07 23:27:06 +0530

    add PurchaseService interface for managing purchase operations

---
+++ src/main/java/services/PurchaseService.java
@@ -0,0 +1,14 @@
+package services;
+
+import model.dto.Purchases;
+
+import java.util.List;
+
+public interface PurchaseService {
+    boolean addPurchase(Purchases purchases);
+    boolean updatePurchase(Purchases purchases);
+    List<Purchases>getAllPurchases();
+    Purchases findPurchaseById(int id);
+    Boolean deletePurchase(String invoiceno);
+
+}
