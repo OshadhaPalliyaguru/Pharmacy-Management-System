49fbb040cba1f014caac8e32e9bb8b290d0ae203
Oshadha <moshadha23@gmail.com>
2026-01-07 23:27:42 +0530

    add SupplierService interface for managing supplier operations

---
+++ src/main/java/services/SupplierService.java
@@ -0,0 +1,16 @@
+package services;
+
+import model.dto.Suppliers;
+
+import java.util.List;
+
+public interface SupplierService {
+    boolean addSupplier(Suppliers suppliers);
+    boolean updateSupplier(Suppliers suppliers);
+    List<Suppliers> getAllSuppliers();
+    Suppliers findByName(String name);
+    boolean deleteSupplier(String id);
+
+
+
+}
