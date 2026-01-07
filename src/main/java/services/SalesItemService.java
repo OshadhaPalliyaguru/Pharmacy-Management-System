ed647d80266d258ad8f9e369e758c6fa2a59936e
Oshadha <moshadha23@gmail.com>
2026-01-07 23:27:31 +0530

    add SalesItemService interface for managing sale items

---
+++ src/main/java/services/SalesItemService.java
@@ -0,0 +1,14 @@
+package services;
+
+import javafx.collections.ObservableList;
+import model.dto.CartItem;
+import model.dto.SaleItem;
+import model.dto.Sales;
+
+import java.util.List;
+
+public interface SalesItemService {
+    List<SaleItem>getAllSaleItems();
+    void addSaleItem(Sales newSaleItem, ObservableList<CartItem>cartItems);
+
+}
