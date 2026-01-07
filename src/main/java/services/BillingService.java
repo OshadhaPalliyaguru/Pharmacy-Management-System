fdd52019bf59338f4e169316065cec30f4acc177
Oshadha <moshadha23@gmail.com>
2026-01-07 23:25:54 +0530

    add BillingService interface for handling billing operations

---
+++ src/main/java/services/BillingService.java
@@ -0,0 +1,13 @@
+package services;
+
+import javafx.collections.ObservableList;
+import model.dto.CartItem;
+import model.dto.Medicine;
+import model.dto.Sales;
+
+public interface BillingService {
+    ObservableList<Medicine> searchByName(String name);
+    void billing(Sales sales, ObservableList<CartItem>cartItems);
+
+
+}
